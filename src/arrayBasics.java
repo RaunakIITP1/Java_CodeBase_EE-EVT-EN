eddb1167687511624948384a8bfeeb981517364e
Raunak Kumar <raunak.iitp@zohomail.in>
2026-02-02 13:48:08 +0530

    Array Basics

---
+++ src/arrayBasics.java
@@ -0,0 +1,14 @@
+public class arrayBasics {
+
+    static void main(String[] args) {
+//        int x=98;
+//        System.out.println(x);
+
+        int[] a = {2, 3 , 4 , 5, 6};
+
+        System.out.println(a[3]);
+
+        a[4]= 10;
+        System.out.println(a[4]);
+    }
+}
